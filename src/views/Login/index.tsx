ace9cf4aa47bf54f604f681a8c4203f2e03fae49
Filipe Tavares <filipe.tavares@pipedrive.com>
2020-04-28 22:43:04 +0100

    Update env vars

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../../assets/logo.png";
 import "./index.css";
 
-const apiUrl = process.env.API_URL || "http://localhost:3005";
+const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:3005";
 
 function Login(props: any) {
   return (
